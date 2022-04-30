b2b7c39e47f12ddb4a5bbabf5d4518154116a532
JoePrinzevalli <joeprinzevalli123@gmail.com>
2022-04-30 13:11:05 -0400

    working on error handlers

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
-// require express and data.json file
-const express = require('express');//use require or import?? may cause probleems laters//
+// Require express and data.json file
+const express = require('express');
 const app = express();
 const {projects} = require('./data/data.json');
 
-// sets view engine to pug
+// SSets view engine to pug
 app.set('view engine', 'pug');
 
-// serves static files located in public folder
+// Serves static files located in public folder
 app.use('/static', express.static('public'));
 
 
-// sets routes to home, about and project pages
+// Sets routes to home, about, project, and error pages
 app.get('/', (req, res, next) => {
     res.render('index', {projects});    //did i set the locals right??
     // res.locals = data.projects;
@@ -31,17 +31,23 @@ app.get('/project/:id', (req, res, next) => {
     next();
   });
 
+app.get('/page-not-found', (req, res, next) => {
+  if(res.status(404)) {
+    res.render('page-not-found', {error});
+  }
+  next();
+})
+
 
 // Error handlers: i think these are wrong
 
 // 404 error
 app.use((req, res, next) => {
-    const err = new Error('err');
+    const err = new Error('Page not found');
     err.status = 404;
     err.message = "Oops, we couldn't find the page you were looking for!";
     console.log(err, err.status, 'Handling a 404 error!');
     next(err);
-    throw err;
 });
 
 // Global error
@@ -49,12 +55,10 @@ app.use((req, res, next, err) => {
   err.status = 500;
   err.message = "Oops, it seems like there is a server error!";
   console.log(err.status, err.message, 'Handling a global error!');
+  next(err);
 });
 
-
-
-
-// app to listen on port 3000
+// App to listen on port 3000
 app.listen(3000, () => {
   console.log('app is listening on port 3000!');
 })
